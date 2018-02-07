650bcaf4113387ab6e15d7eac843cf2a4441a205
ericgit2017 <2914608648@qq.com>
2018-02-07 22:44:32 +0800

    add <int:id> ..

--- lflask.py
+++ lflask.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "<h1>hello world!!</h1>"
+@app.route("/user/<int:id>")
+def hello(id):
+    return "<h1>hello id's %s</h1>" % id
 
 if __name__ == "__main__":
     app.run(debug=True)
