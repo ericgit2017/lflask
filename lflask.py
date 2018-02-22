ee3f28e77e3342a88d06c1c70728b34e961b3f83
ericgit2017 <2914608648@qq.com>
2018-02-22 10:15:12 +0800

    modify lflask.py on origin de

--- lflask.py
+++ lflask.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 
 app = Flask(__name__)
 
@@ -6,6 +7,12 @@ app = Flask(__name__)
 @app.route("/user/<int:id>")
 def hello(id):
     return "<h1>hello id's %s</h1>" % id
+
+
+@app.route("/")
+def index():
+    user_agent = request.headers.get("User-Agent")
+    return "<h2>Your brower is:</h2> %s" % user_agent
 
 if __name__ == "__main__":
     app.run(debug=True)
